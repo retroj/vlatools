fc21e537cfe31157bd7152004d5d222ec7f2d8e8
John Foerch <jjfoerch@earthlink.net>
2013-06-12 16:00:52 -0400

    svgpath2vla.scm: reindent

--- svgpath2vla.scm
+++ svgpath2vla.scm
@@ -17,64 +17,64 @@ exec csi -s $0 "$@"
 
 
 (define (svgpath->vla filename)
-(let* ((svg (with-input-from-file filename
-              (lambda () (ssax:xml->sxml (current-input-port) '()))))
-       (paths (map
-               (lambda (p)
-                 (map
-                  (lambda (s) (with-input-from-string s read))
-                  (string-tokenize
-                   (second p)
-                   (char-set-delete char-set:graphic #\,))))
-               ((sxpath '(// http://www.w3.org/2000/svg:path @ d)) svg)))
-       (x 0)
-       (y 0)
-       (mode 'line)
-       (curve-coords 0))
+  (let* ((svg (with-input-from-file filename
+                (lambda () (ssax:xml->sxml (current-input-port) '()))))
+         (paths (map
+                 (lambda (p)
+                   (map
+                    (lambda (s) (with-input-from-string s read))
+                    (string-tokenize
+                     (second p)
+                     (char-set-delete char-set:graphic #\,))))
+                 ((sxpath '(// http://www.w3.org/2000/svg:path @ d)) svg)))
+         (x 0)
+         (y 0)
+         (mode 'line)
+         (curve-coords 0))
 
-  (define (curve-mode?)
-    (eq? mode 'curve))
+    (define (curve-mode?)
+      (eq? mode 'curve))
 
-  (define (line-mode?)
-    (eq? mode 'line))
+    (define (line-mode?)
+      (eq? mode 'line))
 
-  (define translate-path
-    (match-lambda*
-     (('m dx dy . more)
-      (set! x (+ x dx))
-      (set! y (+ y dy))
-      (set! mode 'line)
-      (printf "P ~A ~A 0.0~%" x y)
-      (apply translate-path more))
-     (('l . more)
-      (set! mode 'line)
-      (apply translate-path more))
-     (('c . more)
-      (set! mode 'curve)
-      (apply translate-path more))
-     ((dx dy . more)
-      (cond
-       ((line-mode?)
+    (define translate-path
+      (match-lambda*
+       (('m dx dy . more)
         (set! x (+ x dx))
         (set! y (+ y dy))
-        (printf "L ~A ~A 1.0~%" x y))
-       ((curve-mode?)
-        (set! curve-coords (+ 1 curve-coords))
-        (when (= 3 curve-coords)
-          (set! curve-coords 0)
+        (set! mode 'line)
+        (printf "P ~A ~A 0.0~%" x y)
+        (apply translate-path more))
+       (('l . more)
+        (set! mode 'line)
+        (apply translate-path more))
+       (('c . more)
+        (set! mode 'curve)
+        (apply translate-path more))
+       ((dx dy . more)
+        (cond
+         ((line-mode?)
           (set! x (+ x dx))
           (set! y (+ y dy))
-          (printf "L ~A ~A 1.0~%" x y))))
-      (apply translate-path more))
-     (_ #t)))
+          (printf "L ~A ~A 1.0~%" x y))
+         ((curve-mode?)
+          (set! curve-coords (+ 1 curve-coords))
+          (when (= 3 curve-coords)
+            (set! curve-coords 0)
+            (set! x (+ x dx))
+            (set! y (+ y dy))
+            (printf "L ~A ~A 1.0~%" x y))))
+        (apply translate-path more))
+       (_ #t)))
 
-  (for-each
-   (lambda (path)
-     (set! x 0)
-     (set! y 0)
-     (set! curve-coords 0)
-     (apply translate-path path))
-   paths)))
+    (for-each
+     (lambda (path)
+       (set! x 0)
+       (set! y 0)
+       (set! curve-coords 0)
+       (apply translate-path path))
+     paths)))
 
 (define opts
   (list))
