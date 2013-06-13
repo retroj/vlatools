0ddc19ad8ac741df936a1c64c9e6dda3a3fe5b58
John Foerch <jjfoerch@earthlink.net>
2013-06-12 23:15:31 -0400

    svgpaths2vla: --fit command-line option to scale figure

--- svgpaths2vla.scm
+++ svgpaths2vla.scm
@@ -11,12 +11,37 @@ exec csi -s $0 "$@"
      extras
      matchable
      (only ports with-output-to-port)
+     regex
      ssax
      sxpath
      utils)
 
 (define (read-from-string s)
   (with-input-from-string s read))
+
+(define (parse-geometry spec)
+  (and-let* ((m (string-match
+                 '(: ($ (+ num)) "x" ($ (+ num))
+                     (? ($ ("+-") (+ num)) ($ ("+-") (+ num))))
+                 spec)))
+    (map (lambda (s) (if s (read-from-string s) s))
+         (cdr m))))
+
+(define fit-wid (make-parameter #f))
+(define fit-hei (make-parameter #f))
+(define fit-ofx (make-parameter #f))
+(define fit-ofy (make-parameter #f))
+
+(define (set-fit-geometry! spec)
+  (cond
+   ((and spec (parse-geometry spec))
+    => (lambda (g)
+         (fit-wid (first g))
+         (fit-hei (second g))
+         (fit-ofx (third g))
+         (fit-ofy (fourth g))))
+   (spec (terminate (sprintf "Failed to parse geometry ~S" spec)))
+   (else val)))
 
 (define header-file (make-parameter #f))
 
@@ -81,19 +106,50 @@ exec csi -s $0 "$@"
 
       (apply the-loop path))))
 
-(define (vla-printer type x y)
-  (case type
-   ((point) (printf "P ~A ~A 0.0~%" x y))
-   ((line) (printf "L ~A ~A 1.0~%" x y))))
-
 (define (svgpaths->vla svg-sxml)
-  (when (header-file)
-    (print* (read-all (header-file))))
-  (for-each
-   (lambda (path) (svgpath->vla path vla-printer))
-   (map svg-break-path
-        ((sxpath '(// "svg:path" @ d "text()") xml-namespaces)
-         svg-sxml))))
+  (let ((paths (map svg-break-path
+                    ((sxpath '(// "svg:path" @ d "text()") xml-namespaces)
+                     svg-sxml)))
+        (minx #f) (miny #f) (maxx #f) (maxy #f)
+        (extents-wid #f) (extents-hei #f))
+
+    (define (extents-calculator _ x y)
+      (set! minx (min x (or minx x)))
+      (set! miny (min y (or miny y)))
+      (set! maxx (max x (or maxx x)))
+      (set! maxy (max y (or maxy y))))
+
+    (define (adjust-x x)
+      (+ (* (- x (if (fit-ofx) minx 0))
+            (if extents-wid (/ (fit-wid) extents-wid) 1))
+         (or (fit-ofx) 0)))
+
+    (define (adjust-y y)
+      (+ (* (- y (if (fit-ofy) miny 0))
+            (if extents-hei (/ (fit-hei) extents-hei) 1))
+         (or (fit-ofy) 0)))
+
+    (define (vla-printer type x y)
+      (let ((x (adjust-x x))
+            (y (adjust-y y)))
+        (case type
+          ((point) (printf "P ~A ~A 0.0~%" x y))
+          ((line) (printf "L ~A ~A 1.0~%" x y)))))
+
+    ;; if fit-geometry has been set, then we must calculate the extents of
+    ;; the figure.
+    (when (fit-wid)
+      (for-each
+       (lambda (path) (svgpath->vla path extents-calculator))
+       paths)
+      (set! extents-wid (- maxx minx))
+      (set! extents-hei (- maxy miny)))
+
+    (when (header-file)
+      (print* (read-all (header-file))))
+    (for-each
+     (lambda (path) (svgpath->vla path vla-printer))
+     paths)))
 
 (define (terminate message #!optional (result 1))
   (with-output-to-port (current-error-port)
@@ -103,6 +159,9 @@ exec csi -s $0 "$@"
 
 (define opts
   (list
+   (args:make-option (fit) (required: "GEOMETRY")
+                     "fit svg paths into GEOMETRY WxH[+X+Y]"
+     (set-fit-geometry! arg))
    (args:make-option (header) (required: "FILE")
                      "include FILE as header in output"
      (header-file arg))))
